070cc5104beb004910d55be7e9ae1db1542ea63c
fluongo <fluongo@gmail.com>
2019-05-28 17:36:16 -0700

    made updates

--- theodore.m
+++ theodore.m
@@ -22,7 +22,7 @@ function varargout = theodore(varargin)
 
 % Edit the above text to modify the response to help theodore
 
-% Last Modified by GUIDE v2.5 19-Apr-2019 09:38:59
+% Last Modified by GUIDE v2.5 28-May-2019 17:35:40
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1376,6 +1376,38 @@ function edit_message_server_delay_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function edit_message_server_delay_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to edit_message_server_delay (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in checkbox_pd.
+function checkbox_pd_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox_pd (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox_pd
+
+
+
+function pd_size_Callback(hObject, eventdata, handles)
+% hObject    handle to pd_size (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of pd_size as text
+%        str2double(get(hObject,'String')) returns contents of pd_size as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function pd_size_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pd_size (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
